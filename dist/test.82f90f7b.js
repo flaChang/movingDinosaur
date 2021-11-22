f63212ece9ccd9bb9ed1247d310e87695724ca3e
flaChang <zza46585@163.com>
2021-11-22 16:02:46 +0800

    another

--- dist/test.82f90f7b.js
+++ dist/test.82f90f7b.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"css.js":[function(require,module,exports) {
+})({"K4Xi":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -127,7 +127,7 @@ exports.default = void 0;
 var string = ".html *,\n.html *::before,\n.html *::after {\n    margin: 0;\n    padding: 0;\n    box-sizing: border-box;\n}\n\nbody {\n    position: relative;\n}\n\n.skin-top {\n    border: 2px solid black;\n    width: 360px;\n    height: 360px;\n    position: relative;\n    left: 50%;\n    margin-left: -180px;\n    border-radius: 60% / 40%;\n    top: 20vh;\n    border-bottom-color: transparent;\n    background-color: rgb(87, 198, 155);\n    z-index: -2;\n}\n\n.skin-top::before {\n    content: '';\n    border: 50px solid blue;\n    border-color: transparent transparent rgb(255, 251, 190) transparent;\n    border-radius: 10px;\n    width: 0px;\n    height: 0px;\n    position: absolute;\n    left: 50%;\n    margin-left: -50px;\n    top: -95px;\n    z-index: 2;\n}\n\n.skin-top::after {\n    content: '';\n    border: 55px solid blue;\n    border-color: transparent transparent black transparent;\n    border-radius: 10px;\n    width: 0px;\n    height: 0px;\n    position: absolute;\n    left: 50%;\n    margin-left: -55px;\n    top: -103px;\n    z-index: 1;\n\n}\n\n.mouth {\n    position: absolute;\n    border: 2px solid black;\n    top: 180px;\n    width: 320px;\n    height: 112px;\n    left: 50%;\n    margin-left: -160px;\n    border-radius: 50%;\n    margin-top: -110px;\n    background-color: rgb(229, 158, 176);\n\n}\n\n.mouth::before {\n    content: \"\";\n    display: block;\n    width: 40px;\n    height: 16px;\n    position: absolute;\n    border-radius: 50%;\n    background-color: rgb(253, 189, 205);\n}\n\n.mouth::after {\n    content: \"\";\n    display: block;\n    width: 40px;\n    height: 16px;\n    position: absolute;\n    border-radius: 50%;\n    left: 100%;\n    margin-left: -42px;\n    background-color: rgb(253, 189, 205);\n}\n\n.teeth1 {\n    position: absolute;\n    border: 2px solid black;\n    width: 50px;\n    height: 25px;\n    border-radius: 0 0 100px 100px;\n    background: #fff;\n    left: 30%;\n    top: 74px;\n    transform: rotate(-7deg);\n}\n\n.teeth2 {\n    position: absolute;\n    border: 2px solid black;\n    width: 50px;\n    height: 25px;\n    border-radius: 0 0 100px 100px;\n    background: #fff;\n    left: 56%;\n    top: 74px;\n    transform: rotate(7deg);\n}\n\n.teeth3 {\n    position: absolute;\n    border: 2px solid black;\n    width: 38px;\n    height: 20px;\n    border-radius: 0 0 50px 50px;\n    background: #fff;\n    left: 10%;\n    top: 93px;\n    transform: rotate(-27deg);\n}\n\n.teeth4 {\n    position: absolute;\n    border: 2px solid black;\n    width: 38px;\n    height: 20px;\n    border-radius: 0 0 50px 50px;\n    background: #fff;\n    left: 80%;\n    top: 94px;\n    transform: rotate(27deg);\n}\n\n.teeth5 {\n    position: absolute;\n    border: 2px solid black;\n    width: 38px;\n    height: 20px;\n    border-radius: 50px 50px 0 0;\n    background: #fff;\n    left: 10%;\n    top: 138px;\n    transform: rotate(27deg);\n}\n\n.teeth6 {\n    position: absolute;\n    border: 2px solid black;\n    width: 50px;\n    height: 25px;\n    border-radius: 100px 100px 0 0;\n    background: #fff;\n    left: 30%;\n    top: 153px;\n    transform: rotate(7deg);\n}\n\n.teeth7 {\n    position: absolute;\n    border: 2px solid black;\n    width: 50px;\n    height: 25px;\n    border-radius: 100px 100px 0 0;\n    background: #fff;\n    left: 56%;\n    top: 153px;\n    transform: rotate(-7deg);\n}\n\n.teeth8 {\n    position: absolute;\n    border: 2px solid black;\n    width: 38px;\n    height: 20px;\n    border-radius: 50px 50px 0 0;\n    background: #fff;\n    left: 80%;\n    top: 138px;\n    transform: rotate(-27deg);\n}\n\n.skin-top .eye-left {\n    position: absolute;\n    border: 3px solid black;\n    border-radius: 0 0 0 50px;\n    border-top-color: transparent;\n    border-right-color: transparent;\n    width: 15px;\n    height: 15px;\n    left: 20%;\n    top: 35px;\n    transform: rotate(100deg);\n}\n\n.skin-top .eye-right {\n    position: absolute;\n    border: 3px solid black;\n    border-radius: 0 0 0 50px;\n    border-top-color: transparent;\n    border-right-color: transparent;\n    width: 15px;\n    height: 15px;\n    left: 76%;\n    top: 35px;\n    transform: rotate(165deg);\n}\n\n.skin-bottom {\n    width: 406px;\n    height: 200px;\n    border-radius: 20% / 60%;\n    border: 2px solid black;\n    position: relative;\n    left: 50%;\n    margin-top: -3%;\n    margin-left: -203px;\n    border-top-color: transparent;\n    border-bottom-color: transparent;\n    background-color: rgb(87, 198, 155);\n    z-index: -2;\n}\n\n.skin-bottom::before {\n    content: \"\";\n    width: 65px;\n    display: block;\n    height: 100px;\n    position: absolute;\n    border: 2px solid black;\n    border-radius: 0 0 20px 50px;\n    background-color: rgb(87, 198, 155);\n    border-top-color: transparent;\n    top: -14%;\n    left: 25%;\n    transform: rotate(-90deg);\n    animation: leftMove 300ms infinite linear;\n}\n\n.skin-bottom::after {\n    content: \"\";\n    display: block;\n    width: 65px;\n    height: 100px;\n    position: absolute;\n    border: 2px solid black;\n    border-radius: 0 0 50px 20px;\n    background-color: rgb(87, 198, 155);\n    border-top-color: transparent;\n    top: -14%;\n    left: 60%;\n    transform: rotate(90deg);\n    animation: rightMove 300ms infinite linear;\n}\n\n.skin-bottom .belly {\n    position: absolute;\n    width: 360px;\n    height: 160px;\n    background: rgb(255, 251, 190);\n    border-radius: 50%;\n    left: 50%;\n    margin-left: -180px;\n    top: 6vh;\n    z-index: -1;\n}\n\nimg{\n    display: block;\n    margin-top: -50%;\n}\n\n@keyframes leftMove {\n    0% {\n        left: 25%;\n    }\n\n    50% {\n        left: 30%;\n    }\n\n    100% {\n        left: 25%;\n    }\n\n}\n@keyframes rightMove {\n    0% {\n        left: 60%;\n    }\n\n    50% {\n        left: 55%;\n    }\n\n    100% {\n        left: 60%;\n    }\n\n}\n";
 var _default = string;
 exports.default = _default;
-},{}],"test.js":[function(require,module,exports) {
+},{}],"HdJB":[function(require,module,exports) {
 "use strict";
 
 var _css = _interopRequireDefault(require("./css.js"));
@@ -137,7 +137,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 //模块化
 var player = {
   id: undefined,
-  n: 1,
   time: 100,
   ui: {
     demo1: document.querySelector('#demo1'),
@@ -151,6 +150,7 @@ var player = {
     '#btnMid': 'normal',
     '#btnFast': 'fast'
   },
+  n: 1,
   init: function init() {
     //初始化
     player.ui.demo1.innerText = _css.default.substr(0, player.n);
@@ -182,6 +182,7 @@ var player = {
     window.clearInterval(player.id);
   },
   play: function play() {
+    window.clearInterval(player.id);
     player.id = setInterval(player.run, player.time); //每次调用play的时候都会把它赋值给id
     //考虑将它内置
   },
@@ -208,208 +209,5 @@ player.init(); //太丑了，如何解决重复的问题
 //使用面向对象的思维再次进行优化
 //每次调用play的时候都会把它赋值给id  考虑将它内置
 //不能在声明一个对象的''
-},{"./css.js":"css.js"}],"C:/Users/5555/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
-var OldModule = module.bundle.Module;
-
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
-    }
-  };
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-var checkedAssets, assetsToAccept;
-var parent = module.bundle.parent;
-
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = "" || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9179" + '/');
-
-  ws.onmessage = function (event) {
-    checkedAssets = {};
-    assetsToAccept = [];
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      var handled = false;
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
-
-          if (didAccept) {
-            handled = true;
-          }
-        }
-      }); // Enable HMR for CSS by default.
-
-      handled = handled || data.assets.every(function (asset) {
-        return asset.type === 'css' && asset.generated.js;
-      });
-
-      if (handled) {
-        console.clear();
-        data.assets.forEach(function (asset) {
-          hmrApply(global.parcelRequire, asset);
-        });
-        assetsToAccept.forEach(function (v) {
-          hmrAcceptRun(v[0], v[1]);
-        });
-      } else {
-        window.location.reload();
-      }
-    }
-
-    if (data.type === 'reload') {
-      ws.close();
-
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
-
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-      removeErrorOverlay();
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID; // html encode message and stack trace
-
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAcceptCheck(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAcceptCheck(bundle.parent, id);
-  }
-
-  if (checkedAssets[id]) {
-    return;
-  }
-
-  checkedAssets[id] = true;
-  var cached = bundle.cache[id];
-  assetsToAccept.push([bundle, id]);
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAcceptCheck(global.parcelRequire, id);
-  });
-}
-
-function hmrAcceptRun(bundle, id) {
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-  cached = bundle.cache[id];
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-
-    return true;
-  }
-}
-},{}]},{},["C:/Users/5555/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","test.js"], null)
-//# sourceMappingURL=/test.e98b79dd.js.map
+},{"./css.js":"K4Xi"}]},{},["HdJB"], null)
+//# sourceMappingURL=test.82f90f7b.js.map
